be20057d2373182563f884fbd89716116eae69a3
colmedo <caolmech@gmail.com>
2017-02-03 11:32:26 +0100

    XML Attributes

--- playgroundsdk/src/main/java/es/grupogo/playgroundsdk/DoButton.java
+++ playgroundsdk/src/main/java/es/grupogo/playgroundsdk/DoButton.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.PopupMenu;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -19,6 +21,9 @@ import android.widget.Toast;
 
 import java.util.List;
 
+import es.grupogo.awesomelibrary.utils.DrawableUtils;
+import es.grupogo.playgroundsdk2.R;
+
 /**
  * Created by Carlos Olmedo on 30/1/17.
  */
@@ -27,30 +32,49 @@ public class DoButton extends ImageView implements View.OnClickListener, Request
 
     private List<Action> actions;
     private String query = "";
+    private int color = Color.BLACK;
 
     public DoButton(Context context) {
         super(context);
-        init();
+        init(null);
     }
 
     public DoButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(attrs);
     }
 
     public DoButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(attrs);
     }
 
 
-    private void init() {
+    private void init(AttributeSet attrs) {
         setOnClickListener(this);
-        setImageResource(es.grupogo.playgroundsdk2.R.drawable.do_24dp);
+
+        //Enable saveInstance
+        setSaveEnabled(true);
+
+        if (attrs!=null) {
+            // Go through all custom attrs.
+            TypedArray attrsArray = getContext().obtainStyledAttributes(attrs, R.styleable.DoButton);
+
+            color = attrsArray.getColor(R.styleable.DoButton_tint, Color.BLACK);
+            query = attrsArray.getString(R.styleable.DoButton_query);
+            if (query==null) {
+                query = "";
+            }
+
+            // Google tells us to call recycle.
+            attrsArray.recycle();
+        }
+
+        setDrawableTint(color);
 
         if (Build.VERSION.SDK_INT >= 16){
-            int[] attrs = new int[] { android.R.attr.selectableItemBackground};
-            TypedArray ta = getContext().obtainStyledAttributes(attrs);
+            int[] atributes = new int[] { android.R.attr.selectableItemBackground};
+            TypedArray ta = getContext().obtainStyledAttributes(atributes);
             Drawable drawableFromTheme = ta.getDrawable(0);
             ta.recycle();
             setBackground(drawableFromTheme);
@@ -58,6 +82,13 @@ public class DoButton extends ImageView implements View.OnClickListener, Request
 
         setScaleType(ScaleType.CENTER);
 
+    }
+
+    public void setDrawableTint(int color) {
+        Drawable d = ContextCompat.getDrawable(getContext(), R.drawable.do_24dp);
+        DrawableUtils.tintDrawable(d, color);
+        setImageDrawable(d);
+        invalidate();
     }
 
     public void setQuery(String query) {
